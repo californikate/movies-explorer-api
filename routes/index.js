5adfb5be087191c544878c6903016363ff80a09a
californikate <regina.plalange8@yandex.ru>
2023-08-16 21:43:06 +0500

    добавляет мидлвэр авторизации

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,13 @@ const router = require('express').Router();
 const authRouter = require('./auth');
 const usersRouter = require('./users');
 const moviesRouter = require('./movies');
+const authMiddle = require('../middlewares/auth');
 
 const NotFoundError = require('../errors/NotFoundError');
 
 router.use(authRouter);
 
+router.use(authMiddle);
 router.use(usersRouter);
 router.use(moviesRouter);
 
