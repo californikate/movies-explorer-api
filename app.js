d5049e03108c494dd92fe85d6f7e7d787711b3b9
californikate <regina.plalange8@yandex.ru>
2023-08-18 16:21:34 +0500

    добавляет пакет cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const limiter = require('./middlewares/limiter');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { DEV_DB_URL } = require('./utils/config');
@@ -13,7 +14,7 @@ const errorsMiddle = require('./middlewares/errors');
 const { PORT = 3000 } = process.env;
 
 const app = express();
-
+app.use(cors());
 mongoose.connect(DEV_DB_URL);
 app.use(helmet());
 
